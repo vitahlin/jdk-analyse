04854b4c425f06b998b8ac9290d96abdcaba3c82
Vitah <linw1225@gmail.com>
2020-06-29 15:19:07 +0800

    Test：HashMapTest

---
+++ jdk8/test/test/java/util/HashMapTest.java
@@ -0,0 +1,11 @@
+package test.java.util;
+
+import java.util.HashMap;
+
+public class HashMapTest {
+    public static void main(String[] args) {
+        HashMap hashMap = new HashMap();
+        hashMap.put("a", "a");
+        System.out.println(hashMap.size());
+    }
+}
