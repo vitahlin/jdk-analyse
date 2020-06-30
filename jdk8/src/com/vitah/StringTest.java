0c011300fe6b24c05a280d35ea7979ee421d607c
Vitah <linw1225@gmail.com>
2020-06-30 12:42:26 +0800

    Feat：删除src/sun/awt和src/sun/nio代码

---
+++ jdk8/src/com/vitah/StringTest.java
@@ -0,0 +1,9 @@
+package com.vitah;
+
+public class StringTest {
+    public static void main(String[] args) {
+        System.out.println("begin");
+        String string = new String();
+        System.out.println(string.length());
+    }
+}
