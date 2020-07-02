0f552815f8a75d0984c3ef3dcfd46a813ae4b1b3
Vitah <linw1225@gmail.com>
2020-07-02 16:38:16 +0800

    Refactor：还原删除的src/sun/awt和src/sun/nio代码

--- jdk8/test/test/java/util/StringTest.java
+++
@@ -1,8 +0,0 @@
-package test.java.util;
-
-public class StringTest {
-    public static void main(String[] args) {
-        String string = new String();
-        System.out.println(string.length());
-    }
-}
